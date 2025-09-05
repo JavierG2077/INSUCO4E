049cd1df3fca312bcfb335cb51884a6d66e93780
JavierG2077 <jgallardoesp@gmail.com>
2025-09-05 17:29:56 -0400

    Add files via upload

---
+++ Clase3JAVA/Pizzeria/src/pizzeria/Pizzeria.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pizzeria;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Duoc
+ */
+public class Pizzeria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        
+    }
+    
+}
